a139c255e7c4a8d52215cce7ee916ae58d012c63
ivangbodnar <ivangbodnar@gmail.com>
2018-09-11 10:06:26 -0300

    Agregado routes.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ThemeListComponent } from './components/theme-list/theme-list.component';
+import {routes} from './routes';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { ThemeListComponent } from './components/theme-list/theme-list.component
     ThemeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
