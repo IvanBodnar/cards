46077642b6cb3b2eb1311addde0248e310e53046
ivangbodnar <ivangbodnar@gmail.com>
2018-09-11 18:56:34 -0300

    Agregada route para card.component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,9 +2,11 @@ import {Routes} from '@angular/router';
 
 import {HomeComponent} from './components/home/home.component';
 import {ThemeListComponent} from './components/theme-list/theme-list.component';
+import {CardComponent} from './components/card-list/card/card.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'themes', component: ThemeListComponent},
+  {path: 'cards/:id', component: CardComponent},
   {path: '', component: HomeComponent}
 ];
