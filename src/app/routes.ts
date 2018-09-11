742e8c25783e2300f21d4b316880fd6fe007bdb7
ivangbodnar <ivangbodnar@gmail.com>
2018-09-11 12:17:12 -0300

    Agregado html básico de theme-list component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,7 +4,7 @@ import {HomeComponent} from './components/home/home.component';
 import {ThemeListComponent} from './components/theme-list/theme-list.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'themes', component: ThemeListComponent}
+  {path: 'themes', component: ThemeListComponent},
+  {path: '', component: HomeComponent}
 ];
