b41071f8f86e9f4d9ed49c935204098ff59031a8
ivangbodnar <ivangbodnar@gmail.com>
2018-09-11 09:33:04 -0300

    Agregado theme-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { ThemeListComponent } from './components/theme-list/theme-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ThemeListComponent
   ],
   imports: [
     BrowserModule
