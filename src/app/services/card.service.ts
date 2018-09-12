0e81e8f3768f966cc54a0c264c4b8555ee0b5e52
ivangbodnar <ivangbodnar@gmail.com>
2018-09-12 17:19:34 -0300

    Se completa CardService y se le agrega getCard

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
+
+import {CardModel} from '../models/card.model';
+import {DataService} from './data.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class CardService {
+export class CardService implements OnInit {
+  cardsArray: CardModel[];
 
-  constructor() { }
+  constructor(
+    private dataService: DataService
+  ) {
+    this.dataService.getCards()
+      .subscribe(
+        (cards: CardModel[]) => {
+          this.cardsArray = cards;
+          console.log(this.cardsArray);
+        }
+      );
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  getCard(index: number) {
+    return this.cardsArray[index];
+  }
 }
