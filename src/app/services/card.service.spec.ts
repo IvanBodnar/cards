d280d82fb2ac73c3482fe00325b65c6a25013e46
ivangbodnar <ivangbodnar@gmail.com>
2018-09-14 20:08:01 -0300

    Tests para CardService

--- src/app/services/card.service.spec.ts
+++ src/app/services/card.service.spec.ts
@@ -1,15 +1,56 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CardService } from './card.service';
+import {of} from 'rxjs';
+import {CardModel} from '../models/card.model';
 
 describe('Cardservice', () => {
+  let cardService: CardService;
+  let mockDataService;
+  let card: CardModel;
+  const CARDS: CardModel[] = [
+    {
+      id: 1,
+      front: 'front',
+      back: 'back',
+      themeId: 1
+    }
+  ];
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [CardService]
     });
   });
 
-  it('should be created', inject([CardService], (service: CardService) => {
-    expect(service).toBeTruthy();
-  }));
+  beforeEach(() => {
+    mockDataService = jasmine.createSpyObj([
+      'getThemes',
+      'getCards'
+    ]);
+
+    mockDataService.getCards.and.returnValue(of(CARDS));
+
+    cardService = new CardService(mockDataService);
+  });
+
+  describe('CardService Creation', () => {
+    it('should be created', () => {
+      expect(cardService).toBeTruthy();
+    });
+  });
+
+  describe('cardsArray', () => {
+    it('should have one CardModel object', () => {
+      expect(cardService.cardsArray.length).toBe(1);
+    });
+  });
+
+  describe('getCard()', () => {
+    it('should return one CardModel object', () => {
+      card = cardService.getCard();
+      expect(card).toEqual(CARDS[0]);
+    });
+  });
+
 });
