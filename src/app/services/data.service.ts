502039ab975df6805876406f3ae52417274408ef
ivanbodnar <ibodnar@outlook.com>
2018-09-17 16:26:16 -0300

    Agregados métodos saveTheme y createTheme en DataService

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from '@angular/common/http';
 
 import {ThemeModel} from '../models/theme.model';
 import {CardModel} from '../models/card.model';
+import {Observable} from 'rxjs';
+import {Headers, RequestOptions} from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +22,18 @@ export class DataService {
 
   getCards() {
     return this.http.get<CardModel[]>(this.url + '/cards');
+  }
+
+  saveTheme(theme: ThemeModel): Observable<ThemeModel> {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
+
+    if (theme.id === 0) {
+      return this.createTheme(theme, options);
+    }
+  }
+
+  createTheme(theme: ThemeModel, options: RequestOptions): Observable<ThemeModel> {
+    return this.http.post<ThemeModel>(this.url + 'themes', options);
   }
 }
