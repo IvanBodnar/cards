c6dd8a15ca0005f2832c78e3152b06ccb7742306
ivangbodnar <ivangbodnar@gmail.com>
2018-09-17 14:18:51 -0300

    Agregada route para ThemeEditComponent

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,10 +3,12 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {ThemeListComponent} from './components/theme-list/theme-list.component';
 import {CardComponent} from './components/card-list/card/card.component';
+import {ThemeEditComponent} from './components/theme-list/theme-edit/theme-edit.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'themes', component: ThemeListComponent},
   {path: 'cards/:id', component: CardComponent},
+  {path: 'theme/:id', component: ThemeEditComponent},
   {path: '', component: HomeComponent}
 ];
