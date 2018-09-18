34d5e4ac217e83dec5fa42a17afe1c838fe8f5f9
ivanbodnar <ibodnar@outlook.com>
2018-09-18 19:00:12 -0300

    Agregados tests para DataService.saveTheme -> POST y PUT

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed, inject } from '@angular/core/testing';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {TestBed, inject, async} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController, TestRequest} from '@angular/common/http/testing';
 
 import { DataService } from './data.service';
 import {CardModel} from '../models/card.model';
+import {ThemeModel} from '../models/theme.model';
+import {HttpHeaders} from '@angular/common/http';
 
 
 describe('DataService', () => {
@@ -59,6 +61,32 @@ describe('DataService', () => {
           themeId: 1
         }
       ] as CardModel[]);
+    });
+  });
+
+  describe('saveTheme', () => {
+    it('should return a ThemeModel object when a POST is made', () => {
+      const data: ThemeModel = {id: undefined, name: 'test'};
+      service.saveTheme(data).subscribe(theme => {
+        expect(theme).toEqual({id: 1, name: 'test'} as ThemeModel);
+      });
+
+      const req: TestRequest = httpTestingController.expectOne('http://localhost:3000/themes');
+      expect(req.request.method).toBe('POST');
+
+      req.flush({id: 1, name: 'test'} as ThemeModel);
+    });
+
+    it('should return a ThemeModel object when a PUT is made', () => {
+      const data: ThemeModel = {id: 1, name: 'test'};
+      service.saveTheme(data).subscribe(theme => {
+        expect(theme).toEqual({id: 1, name: 'test'} as ThemeModel);
+      });
+
+      const req: TestRequest = httpTestingController.expectOne('http://localhost:3000/themes/1');
+      expect(req.request.method).toBe('PUT');
+
+      req.flush({id: 1, name: 'test'} as ThemeModel);
     });
   });
 });
