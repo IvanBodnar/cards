5522184b37368e36f7638f4a0b4fe1e82dc7b340
ivanbodnar <ibodnar@outlook.com>
2018-09-19 07:41:07 -0300

    Agregados botones para agregar tarjetas y volver a temas en theme-edit html con routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,10 +4,12 @@ import {HomeComponent} from './components/home/home.component';
 import {ThemeListComponent} from './components/theme-list/theme-list.component';
 import {CardComponent} from './components/card-list/card/card.component';
 import {ThemeEditComponent} from './components/theme-list/theme-edit/theme-edit.component';
+import {CardListComponent} from './components/card-list/card-list.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'themes', component: ThemeListComponent},
+  {path: 'cards', component: CardListComponent},
   {path: 'cards/:id', component: CardComponent},
   {path: 'theme/:id/edit', component: ThemeEditComponent},
   {path: '', component: HomeComponent}
