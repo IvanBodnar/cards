a55f59127508f9b18eaf5768bb606334fd6edf28
ivanbodnar <ibodnar@outlook.com>
2018-09-20 19:16:30 -0300

    Agregado FormGroup a card-edit.component

--- src/app/components/card-edit/card-edit.component.ts
+++ src/app/components/card-edit/card-edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import {DataService} from '../../services/data.service';
+
+import {State} from '../../shared/enums';
 
 @Component({
   selector: 'app-card-edit',
@@ -8,15 +12,23 @@ import {FormGroup} from '@angular/forms';
 })
 export class CardEditComponent implements OnInit {
   cardForm: FormGroup;
+  state: State;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) { }
 
   ngOnInit() {
     // $ declarada globalmente en global.d.ts
     // @ts-ignore
-    $('.menu .item')
-      .tab()
-    ;
+    $('.menu .item').tab();
+
+
+    this.cardForm = new FormGroup({
+      front: new FormControl(),
+      back: new FormControl(),
+    });
   }
 
 }
