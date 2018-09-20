8c2b193926bce4287127e60dcccf0d244f07ed50
ivanbodnar <ibodnar@outlook.com>
2018-09-20 16:45:34 -0300

    Actualizados tests para card.component

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,17 +1,20 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
+import {of} from 'rxjs';
 
 import { CardComponent } from './card.component';
 import {CardModel} from '../../models/card.model';
 import {CardService} from '../../services/card.service';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {DataService} from '../../services/data.service';
 
 
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
   let mockCardService;
+  let mockDataService;
   const CARD: CardModel = {
     id: 1,
     front: 'front',
@@ -19,14 +22,20 @@ describe('CardComponent', () => {
     themeId: 1
   };
 
+  const CARDS: CardModel[] = [CARD];
+
   beforeEach(async(() => {
     mockCardService = jasmine.createSpyObj(['getCard']);
+    mockDataService = jasmine.createSpyObj(['getCards']);
     mockCardService.getCard.and.returnValue(CARD);
+    mockDataService.getCards.and.returnValue(of(CARDS));
+
 
     TestBed.configureTestingModule({
       declarations: [ CardComponent ],
       providers: [
-        { provide: CardService, useValue: mockCardService }
+        { provide: CardService, useValue: mockCardService },
+        { provide: DataService, useValue: mockDataService }
       ],
       imports: [
         HttpClientTestingModule,
@@ -46,6 +55,10 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should populate cardsArray when created', () => {
+    expect(component.cardsArray).toEqual(CARDS);
   });
 
   it('should change side when flip button is clicked', () => {
@@ -69,7 +82,7 @@ describe('CardComponent', () => {
     const debugEl = fixture.debugElement.query(By.css('a'));
     debugEl.triggerEventHandler('click', null);
     expect(component.frontSide).toBe(true);
-    expect(mockCardService.getCard).toHaveBeenCalledTimes(1);
+    expect(mockCardService.getCard).toHaveBeenCalledTimes(2);
 
     // Card body
     fixture.detectChanges();
