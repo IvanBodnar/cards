7d5093de0571efc3ae9d1882161a7e6346c69261
ivanbodnar <ibodnar@outlook.com>
2018-09-24 19:37:20 -0300

    Agregados tests para theme-list

--- src/app/components/theme-list/theme-list.component.spec.ts
+++ src/app/components/theme-list/theme-list.component.spec.ts
@@ -1,25 +1,71 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { ThemeListComponent } from './theme-list.component';
-//
-// describe('ThemeListComponent', () => {
-//   let component: ThemeListComponent;
-//   let fixture: ComponentFixture<ThemeListComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ThemeListComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ThemeListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import { ThemeListComponent } from './theme-list.component';
+import {DataService} from '../../services/data.service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {DebugElement, Provider} from '@angular/core';
+import {ThemeModel} from '../../models/theme.model';
+import {of} from 'rxjs';
+import {By} from '@angular/platform-browser';
+
+describe('ThemeListComponent', () => {
+  let component: ThemeListComponent;
+  let fixture: ComponentFixture<ThemeListComponent>;
+  let mockDataService;
+  const THEMES: ThemeModel[] = [
+    {
+      id: 1,
+      name: 'name'
+    } as ThemeModel
+  ];
+
+  beforeEach(async(() => {
+    mockDataService = jasmine.createSpyObj(['getThemes', 'deleteTheme']);
+    mockDataService.getThemes.and.returnValue(of(THEMES));
+    mockDataService.deleteTheme.and.returnValue(of(THEMES[0] as ThemeModel));
+
+    TestBed.configureTestingModule({
+      declarations: [ ThemeListComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        <Provider>{ provide: DataService, useValue: mockDataService }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ThemeListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call dataService.getThemes 1 time when created', () => {
+    expect(mockDataService.getThemes).toHaveBeenCalledTimes(1);
+  });
+
+  it('should populate attribute themesArray when is created', () => {
+    expect(component.themesArray[0]).toEqual({id: 1, name: 'name'});
+  });
+
+  it('should create 1 item on html', () => {
+    const themeItem: DebugElement[] = fixture.debugElement.queryAll(By.css('.item'));
+    expect(themeItem.length).toEqual(1);
+  });
+
+  it('should call onDelete -> dataService.deleteTheme 1 time -> dataService.getThemes 2 times when delete icon is clicked', () => {
+    const deleteIcon: DebugElement = fixture.debugElement.query(By.css('#a-delete'));
+    deleteIcon.triggerEventHandler('click', THEMES[0]);
+
+    expect(mockDataService.deleteTheme).toHaveBeenCalledTimes(1);
+    expect(mockDataService.getThemes).toHaveBeenCalledTimes(2);
+  });
+});
